87c87da43d77f8559f7976a63fc2019fe0ec46a1
Bobelle <184843334+Bobelle-Homebase@users.noreply.github.com>
2026-04-05 11:57:33 +0200

    Update Dashboard Credits Counter (Bielefeld Edition)-v1.0.31.user.js
    Update Dashboard Credits Counter (Bielefeld Edition)-v1.0.31.user.js

--- Dashboard Credits Counter (Bielefeld Edition)-v1.0.29.user.js
+++ Dashboard Credits Counter (Bielefeld Edition)-v1.0.29.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dashboard Credits Counter (Bielefeld Edition)
 // @namespace    https://leitstellenspiel.de/bielefeld
-// @version      v1.0.30
+// @version      v1.0.31
 // @license      Design by Bobelle
 // @author       Design by Bobelle
 // @description  Zeigt Tageszähler, Credits-Bilanz und Dienstgrad im Navbar-Bereich
