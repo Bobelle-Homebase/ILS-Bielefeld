3fc2eeff1e0a5efe60836167fdb136555e46b872
Bobelle <184843334+Bobelle-Homebase@users.noreply.github.com>
2026-04-05 11:48:26 +0200

    Update Dashboard Eigenes Logo v1.0.30.user.js
    Update Dashboard Eigenes Logo v1.0.30.user.js

--- Dashboard Eigenes Logo (Bielefeld Edition)-v1.0.29.user.js
+++ Dashboard Eigenes Logo (Bielefeld Edition)-v1.0.29.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Dashboard Eigenes Logo (Bielefeld Edition)
 // @namespace    https://leitstellenspiel.de/bielefeld
-// @version      v1.0.29
+// @version      v1.0.30
 // @license      Design by Bobelle
 // @author       Design by Bobelle
 // @description  Blendet das Original-Logo aus und legt ein eigenes darüber
-// @updateURL    https://github.com/Bobelle-Homebase/ILS-Bielefeld/raw/refs/heads/main/Dashboard.User.js
-// @downloadURL  https://github.com/Bobelle-Homebase/ILS-Bielefeld/raw/refs/heads/main/Dashboard.User.js
+// @updateURL    https://github.com/Bobelle-Homebase/ILS-Bielefeld/raw/refs/heads/main/Dashboard%20Eigenes%20Logo%20(Bielefeld%20Edition)-v1.0.29.user.js
+// @downloadURL  https://github.com/Bobelle-Homebase/ILS-Bielefeld/raw/refs/heads/main/Dashboard%20Eigenes%20Logo%20(Bielefeld%20Edition)-v1.0.29.user.js
 // @icon         https://www.leitstellenspiel.de/favicon.ico
 // @match        https://www.leitstellenspiel.de/*
 // @grant        GM_setValue
